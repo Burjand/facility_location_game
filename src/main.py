32b841d5608af85ee4dc559ea082e012ed9e977b
Andrés Burjand Torres Reyes <andresbur.torresreyes@gmail.com>
2025-05-05 23:40:45 -0600

    Minor changes

--- src/main.py
+++ src/main.py
@@ -7,13 +7,16 @@ if __name__ == "__main__":
     n_potential_facilities = 80 # Number of potential facilities
     n_brd_players = 10 # Number of players in the BRD process
     max_iterations = 1000 # Max number of iterations before stopping an episode
-    #convergence_threshold = 1e-5 # Threshold used to determine convergence of the potential function (Not used in this version of the code)
     seed = 66
     n_simulations = 10 # Number of simulations to run
+    #convergence_threshold = 1e-5 # Threshold used to determine convergence of the potential function (Not used in this version of the code)
+    
 
     # SIMULATION
+
     # Setup simulation
     simulation = Simulation(n_nodes, n_potential_facilities, n_brd_players, max_iterations, n_simulations, seed)
+    
     # Run simulations
     if n_simulations == 1:
         iterations, potential_function_development, players_development_over_time = simulation.run_FLG_BRD_simulation()
